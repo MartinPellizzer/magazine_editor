e17410dbce5b62c3ec98985f44e1be3f84b33343
Martin Pellizzer <martinpellizzer@gmail.com>
2024-06-09 16:20:20 +0200

    '

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ page_y = window_h//2-page_h//2
 
 # FLAGS
 flag_a4_grid = 0
+flag_brush_type = 't'
 
 
 
@@ -100,7 +101,7 @@ grid = []
 for row_i in range(row_num):
     row_curr = []
     for col_i in range(col_num):
-        row_curr.append(0)
+        row_curr.append('')
     grid.append(row_curr)
 
 screen = pygame.display.set_mode((window_w, window_h), 0, 32)
@@ -213,14 +214,14 @@ def preview_page():
             draw.line((x_1, y_1, x_2, y_2), fill='#ff00ff', width=4)
 
     # draw image
-    col_i_x_1 = -1
-    col_i_y_1 = -1
-    col_i_x_2 = -1
-    col_i_y_2 = -1
+    col_i_x_1 = ''
+    col_i_y_1 = ''
+    col_i_x_2 = ''
+    col_i_y_2 = ''
     is_first_pos = True
     for row_i in range(row_num):
         for col_i in range(col_num):
-            if grid[row_i][col_i] == 1:
+            if grid[row_i][col_i] == 'i':
                 if is_first_pos: 
                     col_i_x_1 = col_i
                     col_i_y_1 = row_i
@@ -229,7 +230,7 @@ def preview_page():
                     col_i_x_2 = col_i
                     col_i_y_2 = row_i
 
-    if col_i_x_1 != -1:
+    if col_i_x_1 != '':
         x_1 = pdf_col_w * col_i_x_1 + pdf_col_pad_x
         y_1 = pdf_row_h * col_i_y_1 + pdf_row_pad_y
         x_2 = pdf_col_w * (col_i_x_2 + 1) + pdf_col_pad_x
@@ -246,61 +247,100 @@ def preview_page():
     with open('placeholder_text.txt', 'r', encoding='utf-8', errors='ignore') as f: text = f.read()
     text = text.replace('\n', ' ')
 
-    # grid to blocks (text in empty cells)
-    blocks_list = []
-    block_curr = [-1, -1, -1]
-    for k in range(col_num):
-        for i in range(row_num):
-            if grid[i][k] == 0:
-                if block_curr[0] == -1: block_curr[0] = i
-                if block_curr[1] == -1: block_curr[1] = k
-            elif grid[i][k] == 1:
-                if block_curr != [-1, -1, -1]:
-                    if block_curr[2] == -1: block_curr[2] = i
-                    blocks_list.append(block_curr)
-                    block_curr = [-1, -1, -1]
-        if block_curr != [-1, -1, -1]:
-            if block_curr[2] == -1: block_curr[2] = i
-            blocks_list.append(block_curr)
-            block_curr = [-1, -1, -1]
-
-    # split lines
-    words = text.split(' ')
-    lines = []
-    line_curr = ''
-    for word in words:
-        _, _, line_curr_w, _ = body_font.getbbox(line_curr)
-        _, _, word_w, _ = body_font.getbbox(word)
-        if line_curr_w + word_w < pdf_col_w - grid_col_gap*2:
-            line_curr += f'{word} '
-        else:
-            lines.append(line_curr)
-            line_curr = f'{word} '
-    lines.append(line_curr)
-
+    
     # draw text
-    block_i = 0
-    start_row_i = blocks_list[block_i][0]
-    start_col_i = blocks_list[block_i][1]
-    end_row_i = blocks_list[block_i][2]
-    line_i = 0
-    for line in lines:
-        x_1 = pdf_col_w * start_col_i + pdf_col_pad_x + grid_col_gap * start_col_i
-        y_1 = pdf_row_h * start_row_i + pdf_row_pad_y + body_font_size * line_i
-        line_row_i = (y_1 - pdf_row_pad_y) // pdf_row_h
-        if line_row_i > end_row_i - 1: 
-            block_i += 1
-            if block_i < len(blocks_list):
-                start_row_i = blocks_list[block_i][0]
-                start_col_i = blocks_list[block_i][1]
-                end_row_i = blocks_list[block_i][2]
-                line_i = 0
-                x_1 = pdf_col_w * start_col_i + pdf_col_pad_x + grid_col_gap * start_col_i
-                y_1 = pdf_row_h * start_row_i + pdf_row_pad_y + body_font_size * line_i
+    # col_i_x_1 = ''
+    # col_i_y_1 = ''
+    # col_i_x_2 = ''
+    # col_i_y_2 = ''
+    # is_first_pos = True
+    # for row_i in range(row_num):
+    #     for col_i in range(col_num):
+    #         if grid[row_i][col_i] == 't':
+    #             if is_first_pos: 
+    #                 col_i_x_1 = col_i
+    #                 col_i_y_1 = row_i
+    #                 is_first_pos = False
+    #             else:
+    #                 col_i_x_2 = col_i
+    #                 col_i_y_2 = row_i
+
+    # grid to blocks (text in empty cells)
+    # blocks_list = []
+    # block_curr = [-1, -1, -1]
+    # for k in range(col_num):
+    #     for i in range(row_num):
+    #         if grid[i][k] == 0:
+    #             if block_curr[0] == -1: block_curr[0] = i
+    #             if block_curr[1] == -1: block_curr[1] = k
+    #         elif grid[i][k] == 1:
+    #             if block_curr != [-1, -1, -1]:
+    #                 if block_curr[2] == -1: block_curr[2] = i
+    #                 blocks_list.append(block_curr)
+    #                 block_curr = [-1, -1, -1]
+    #     if block_curr != [-1, -1, -1]:
+    #         if block_curr[2] == -1: block_curr[2] = i
+    #         blocks_list.append(block_curr)
+    #         block_curr = [-1, -1, -1]
+
+    blocks_list = []
+    block_curr = ['', '', '']
+    for col_i in range(col_num):
+        for row_i in range(row_num):
+            if grid[row_i][col_i] == 't':
+                if block_curr[0] == '': block_curr[0] = col_i
+                if block_curr[1] == '': block_curr[1] = row_i
+            elif grid[row_i][col_i] != 't':
+                if block_curr != ['', '', '']:
+                    if block_curr[2] == '': block_curr[2] = row_i
+                    blocks_list.append(block_curr)
+                    block_curr = ['', '', '']
+        if block_curr != ['', '', '']:
+            if block_curr[2] == '': block_curr[2] = row_i
+            blocks_list.append(block_curr)
+            block_curr = block_curr = ['', '', '']
+
+    if blocks_list != []:
+
+        # split lines
+        words = text.split(' ')
+        lines = []
+        line_curr = ''
+        for word in words:
+            _, _, line_curr_w, _ = body_font.getbbox(line_curr)
+            _, _, word_w, _ = body_font.getbbox(word)
+            if line_curr_w + word_w < pdf_col_w - grid_col_gap*2:
+                line_curr += f'{word} '
             else:
-                break
-        draw.text((x_1, y_1), line, (0, 0, 0), font=body_font)
-        line_i += 1
+                lines.append(line_curr)
+                line_curr = f'{word} '
+        lines.append(line_curr)
+
+        print(blocks_list)
+        # draw text
+        block_i = 0
+        start_col_i = blocks_list[block_i][0]
+        start_row_i = blocks_list[block_i][1]
+        end_row_i = blocks_list[block_i][2]
+        line_i = 0
+        for line in lines:
+            x_1 = pdf_col_w * start_col_i + pdf_col_pad_x + grid_col_gap * start_col_i
+            y_1 = pdf_row_h * start_row_i + pdf_row_pad_y + body_font_size * line_i
+            line_row_i = (y_1 - pdf_row_pad_y) // pdf_row_h
+            if line_row_i > end_row_i - 1: 
+                print(f'here:{block_i}')
+                block_i += 1
+                if block_i < len(blocks_list):
+                    start_col_i = blocks_list[block_i][0]
+                    start_row_i = blocks_list[block_i][1]
+                    end_row_i = blocks_list[block_i][2]
+                    line_i = 0
+                    x_1 = pdf_col_w * start_col_i + pdf_col_pad_x + grid_col_gap * start_col_i
+                    y_1 = pdf_row_h * start_row_i + pdf_row_pad_y + body_font_size * line_i
+                else:
+                    break
+            draw.text((x_1, y_1), line, (0, 0, 0), font=body_font)
+            line_i += 1
 
     img.show()
 
@@ -324,13 +364,16 @@ while True:
         if pygame.mouse.get_pressed()[0]:
             mouse_click_pos = pygame.mouse.get_pos()
             mouse_click_col_i, mouse_click_row_i = cell_from_pos(mouse_click_pos)
-            grid[mouse_click_row_i][mouse_click_col_i] = 1
+            if flag_brush_type == 't':
+                grid[mouse_click_row_i][mouse_click_col_i] = 't'
+            if flag_brush_type == 'i':
+                grid[mouse_click_row_i][mouse_click_col_i] = 'i'
             print(grid)
 
         if pygame.mouse.get_pressed()[2]:
             mouse_click_pos = pygame.mouse.get_pos()
             mouse_click_col_i, mouse_click_row_i = cell_from_pos(mouse_click_pos)
-            grid[mouse_click_row_i][mouse_click_col_i] = 0
+            grid[mouse_click_row_i][mouse_click_col_i] = ''
             print(grid)
             
         if event.type == pygame.KEYDOWN:
@@ -341,7 +384,6 @@ while True:
             if event.key == pygame.K_s:
                 with open('map.csv', 'w', newline='') as f:
                     write = csv.writer(f)
-                    # write.writerow(fields)
                     write.writerows(grid)
             # CTRL + G
             if event.key == pygame.K_g and (pygame.key.get_mods() & pygame.KMOD_CTRL):
@@ -351,6 +393,12 @@ while True:
                 show_grid = not show_grid
             if event.key == pygame.K_p:
                 preview_page()
+            
+            if event.key == pygame.K_t:
+                flag_brush_type = 't'
+            if event.key == pygame.K_i:
+                flag_brush_type = 'i'
+            
 
 
     draw_background()
@@ -371,13 +419,19 @@ while True:
     # flags
     text_surface = my_font.render(f'flag_a4_grid:{flag_a4_grid}', False, '#ff00ff')
     screen.blit(text_surface, (0, 120))
+    text_surface = my_font.render(f'flag_brush_type:{flag_brush_type}', False, '#ff00ff')
+    screen.blit(text_surface, (0, 150))
 
     
     for row_i in range(row_num):
         for col_i in range(col_num):
-            if grid[row_i][col_i] == 1:
+            if grid[row_i][col_i] == 'i':
                 red_x_1 = page_x + col_w * col_i + col_pad_x
                 red_y_1 = page_y + row_h * row_i + row_pad_y
                 pygame.draw.rect(screen, '#ff0000', (red_x_1, red_y_1, col_w, row_h))
+            if grid[row_i][col_i] == 't':
+                red_x_1 = page_x + col_w * col_i + col_pad_x
+                red_y_1 = page_y + row_h * row_i + row_pad_y
+                pygame.draw.rect(screen, '#000000', (red_x_1, red_y_1, col_w, row_h))
 
     pygame.display.update()
