8003aa3256a9b1796cefaf0cf83d84c2249063d0
MartinPellizer <martinpellizzer@gmail.com>
2024-09-09 22:50:07 +0200

    '

--- editor_grid_system.py
+++ editor_grid_system.py
@@ -72,7 +72,7 @@ def image_gen(prompt, image_filepath_out):
     image = pipe(prompt=prompt, num_inference_steps=25).images[0]
     image.save(image_filepath_out)
 
-def draw_page(image_i, body_text, place='left', image_regen=False, show_guides=False):
+def draw_page(page_num, body_text, place='left', image_regen=False, show_guides=False):
     if place == 'left':
         text_area_x1 = p1_ml
         text_area_y1 = p1_mt
@@ -115,58 +115,79 @@ def draw_page(image_i, body_text, place='left', image_regen=False, show_guides=F
         draw.line((text_area_x2, text_area_y1, text_area_x2, text_area_y2), fill='#ff0000', width=4)
 
     ## images
-    if image_regen:
-        prompt = f'''
-            milk,
-            food photography,
-            natural light, 
-            depth of field, bokeh,
-            high resolution, cinematic
-        '''
-        image_gen(prompt, 'images/image-1.png')
+    total_cells = 0
+    image_index = 0
+    for image_i in range(99):
+        if image_regen:
+            prompt = f'''
+                milk,
+                food photography,
+                natural light, 
+                depth of field, bokeh,
+                high resolution, cinematic
+            '''
+            image_gen(prompt, 'images/image-1.png')
 
-    foreground = Image.open('images/image-1.png')
-    cell_x1_index = random.randint(0, col_num-1)
-    cell_y1_index = random.randint(0, row_num-1)
-    cell_x2_index = random.randint(cell_x1_index, col_num-1)
-    cell_y2_index = random.randint(cell_y1_index, row_num-1)
-    '''
-    cell_x1_index = 0
-    cell_y1_index = 0
-    cell_x2_index = 0
-    cell_y2_index = 0
-    '''
-    print(f'{cell_x1_index}:{cell_y1_index}')
-    print(f'{cell_x2_index}:{cell_y2_index}')
-    print()
-    for i in range(row_num):
-        for j in range(col_num):
-            if i >= cell_y1_index and j >= cell_x1_index and i <= cell_y2_index and j <= cell_x2_index:
-                map_matrix[i][j] = 'i_0'
+        if image_index == 0:
+            cell_x1_index = random.randint(0, col_num-1)
+            cell_y1_index = random.randint(0, row_num-1)
+            cell_x2_index = random.randint(cell_x1_index, col_num-1)
+            cell_y2_index = random.randint(cell_y1_index, row_num-1)
+        else:
+            available_cells = []
+            for i in range(row_num):
+                for j in range(col_num):
+                    if map_matrix[i][j] == '':
+                        available_cells.append([i, j])
+            random_cell = random.choice(available_cells)
+            cell_x1_index = random_cell[1]
+            cell_y1_index = random_cell[0]
+            cell_x2_index = random_cell[1]
+            cell_y2_index = random_cell[0]
 
-    for row in map_matrix:
-        print(row)
-    
-    x1 = int(text_area_x1 + col_w * cell_x1_index)
-    y1 = int(text_area_y1 + row_h * cell_y1_index)
-    w = int(col_w * (cell_x2_index - cell_x1_index + 1) - line_h)
-    h = int(row_h * (cell_y2_index - cell_y1_index + 1) - line_h - (line_h - line_h/line_spacing))
+        print(f'{cell_x1_index}:{cell_y1_index}')
+        print(f'{cell_x2_index}:{cell_y2_index}')
 
-    util.img_resize_save(
-        'images/image-1.png', 'images/image-1-resized.jpg', 
-        w=w, h=h, 
-        quality=100,
-    )
-    foreground = Image.open('images/image-1-resized.jpg')
-    img.paste(foreground, (x1, y1))
+        selected_cells = (cell_x2_index - cell_x1_index + 1) * (cell_y2_index - cell_y1_index + 1)
+        print(selected_cells)
+        if total_cells == 3: break
+        if total_cells + selected_cells > 3: continue
+        total_cells += selected_cells
+        
+        ## update map_matrix
+        if image_index == 0:
+            for i in range(row_num):
+                for j in range(col_num):
+                    if i >= cell_y1_index and j >= cell_x1_index and i <= cell_y2_index and j <= cell_x2_index:
+                        map_matrix[i][j] = f'i_{image_index}'
+        else:
+            map_matrix[cell_y1_index][cell_x1_index] = f'i_{image_index}'
+        image_index += 1
+        for row in map_matrix:
+            print(row)
+        
+        ## resize/draw image
+        x1 = int(text_area_x1 + col_w * cell_x1_index)
+        y1 = int(text_area_y1 + row_h * cell_y1_index)
+        w = int(col_w * (cell_x2_index - cell_x1_index + 1) - line_h)
+        h = int(row_h * (cell_y2_index - cell_y1_index + 1) - line_h - (line_h - line_h/line_spacing))
+        print(x1, y1, w, h)
+        util.img_resize_save(
+            'images/image-1.png', 'images/image-1-resized.jpg', 
+            w=w, h=h, 
+            quality=100,
+        )
+        foreground = Image.open('images/image-1-resized.jpg')
+        img.paste(foreground, (x1, y1))
+        print()
 
-    ## text blocks
-    blocks = []
-    for i in range(col_num):
-        for j in range(row_num):
-            print(f'col: {i}, row: {j} - {map_matrix[j][i]}')
-            if map_matrix[j][i] == '':
-                blocks.append([i, j])
+        ## text blocks
+        blocks = []
+        for i in range(col_num):
+            for j in range(row_num):
+                # print(f'col: {i}, row: {j} - {map_matrix[j][i]}')
+                if map_matrix[j][i] == '':
+                    blocks.append([i, j])
 
     ## title
     '''
@@ -213,7 +234,6 @@ def draw_page(image_i, body_text, place='left', image_regen=False, show_guides=F
                 line_curr = f'{word} '
         lines.append(line_curr.strip())
 
-    print(blocks)
     if len(blocks) != 0:
         block_index = 0
         block_curr_x, block_curr_y = blocks[block_index]
@@ -284,9 +304,9 @@ def draw_page(image_i, body_text, place='left', image_regen=False, show_guides=F
     y1 = text_area_y2 + (line_h*line_spacing*8)
     draw.text((x1, y1), line, '#000000', font=body_font)
 
-    img.save(f'exports-test/{image_i}.jpg')
-    img.show()
-    quit()
+    img.save(f'exports-test/{page_num}.jpg')
+    # img.show()
+    # quit()
 
 def draw_page_full_image(image_i, place='left', image_regen=False):
     if place == 'left':
